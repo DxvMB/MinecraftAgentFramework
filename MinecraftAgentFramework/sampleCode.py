9be7bfd3ee9665fea0656d56ceab5471e73ec889
kamikazeDulcecito <84016001+kamikazeDulcecito@users.noreply.github.com>
2025-01-28 23:35:47 +0100

    Add files via upload

---
+++ MinecraftAgentFramework/sampleCode.py
@@ -0,0 +1,11 @@
+# Import necessary modules
+import mcpi.minecraft as Minecraft
+import mcpi.block as block
+from MInecraftAgentFramework.mcpi import minecraft
+
+# Connect to the Minecraft game
+mc = minecraft.Minecraft.create()
+# Interact with the Minecraft world
+mc.postToChat("Hello Minecraft World")
+pos = mc.player.getTilePos()
+mc.setBlock(pos.x+3, pos.y, pos.z, block.STONE.id)
