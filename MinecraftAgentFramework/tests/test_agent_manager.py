e273d547d9a1e1e4acbe602571bc87acc96e62d6
kamikazeDulcecito <84016001+kamikazeDulcecito@users.noreply.github.com>
2025-02-02 21:45:19 +0100

    Update test_agent_manager.py

--- MinecraftAgentFramework/tests/test_agent_manager.py
+++ MinecraftAgentFramework/tests/test_agent_manager.py
@@ -1,13 +1,13 @@
 import unittest
 from unittest.mock import patch, MagicMock
-from MinecraftAgentFramework.framework.agent_manager import AgentManager # Importa el módulo que contiene AgentManager
+from MinecraftAgentFramework.framework.agent_manager import AgentManager 
 
 
 class TestAgentManager2(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        # Configura un mock para Minecraft.create() para evitar la conexión real a Minecraft
+        
         cls.mock_minecraft = MagicMock()
         AgentManager.mc = cls.mock_minecraft
 
@@ -19,23 +19,23 @@ class TestAgentManager2(unittest.TestCase):
     @patch.object(AgentManager, "stop_bot")
     @patch.object(AgentManager, "start_bot")
     def test_start_all(self, mock_start, mock_stop, mock_list, mock_minecraft):
-        # Test that all available bots are started
+        
         self.bot_manager.start_all()
         mock_start.assert_any_call("insult_bot")
         mock_start.assert_any_call("oracle_bot")
         mock_start.assert_any_call("tnt_bot")
-        self.assertEqual(mock_start.call_count, 4)  # Asegurarse de que haya 3 hilos creados.
+        self.assertEqual(mock_start.call_count, 4)  
 
     @patch("MinecraftAgentFramework.framework.agent_manager.AgentManager.read")
     @patch("MinecraftAgentFramework.framework.agent_manager.print")
     def test_read_and_response_start(self, mock_print, mock_read):
-        mock_read.side_effect = ["unknown_command", "exit"]  # Agrega "exit" para salir del bucle
+        mock_read.side_effect = ["unknown_command", "exit"]  
         self.bot_manager.read_and_response()
         mock_print.assert_any_call("Comando no reconocido. Intente de nuevo.")
 
 
     def test_stop_bot(self):
-        # Simula un bot en ejecución
+        
         bot_type = "test_bot"
         mock_bot = MagicMock()
         mock_thread = MagicMock()
@@ -43,14 +43,14 @@ class TestAgentManager2(unittest.TestCase):
 
         self.bot_manager.stop_bot(bot_type)
 
-        mock_bot.set_run.assert_called_once()  # Asegúrate de que se llama a set_run()
-        mock_thread.join.assert_called_once()  # Asegúrate de que se llama a join()
+        mock_bot.set_run.assert_called_once()  #  set_run()
+        mock_thread.join.assert_called_once()  #  join()
         self.assertNotIn(bot_type, self.bot_manager.threads)
 
     def test_list_active_bots(self):
-        # Simula algunos bots activos.
+        # bots simulation
         self.bot_manager.threads = {"bot1": {}, "bot2": {}}
-        with patch('builtins.print') as mock_print:  # Mock de print para verificar la salida
+        with patch('builtins.print') as mock_print:  # verify out
             self.bot_manager.list_active_bots()
             mock_print.assert_any_call("Bots activos:")
             mock_print.assert_any_call("- bot1")
